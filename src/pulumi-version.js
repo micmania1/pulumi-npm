3e6eacc266de2dc0076edeceb01ca94410dc73e9
Michael Strong <github@michaelstrong.co.uk>
2022-01-05 14:50:46 +1300

    Enable Github API to be called with an auth token

--- src/pulumi-version.js
+++ src/pulumi-version.js
@@ -3,7 +3,17 @@ const fetch = require('node-fetch');
 const pulumiRepo = 'https://api.github.com/repos/pulumi/pulumi';
 
 module.exports = async function getLatestPulumiVersion() {
-  const tags = await fetch(`${pulumiRepo}/tags`).then(res => res.json());
+  const options = {};
+  if (process.env.GITHUB_TOKEN) {
+    options.headers = {
+      authorization: `token ${process.env.GITHUB_TOKEN}`
+    };
+  }
+
+  const tags = await fetch(`${pulumiRepo}/tags`, options).then(res =>
+    res.json()
+  );
+
   const latestVersion2Tag = tags.filter(tag => tag.name.startsWith('v3'))[0];
   return latestVersion2Tag.name;
 };
